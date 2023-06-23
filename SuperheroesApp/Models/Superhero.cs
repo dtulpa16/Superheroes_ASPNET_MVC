18363b420bc2ce0abfe92095d475d3d4649133a3
Daniel Tulpa <dtulpa16@yahoo.com>
2023-06-23 13:34:17 -0600

    Added Boilerplate MVC Code + Created Superhero Model

---
+++ SuperheroesApp/Models/Superhero.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SuperheroesApp.Models
+{
+    public class Superhero
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string AlterEgo { get; set; }
+
+        public string PrimaryAbility { get; set; }
+
+        public string SecondaryAbility { get; set;}
+
+        public string Catchphrase { get; set; }
+    }
+}
